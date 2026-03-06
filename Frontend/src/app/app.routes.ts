dad8118ca89d16da4c9fa544d0119f85de6b7a39
Nguyen Ho The Anh <theanh2002nguyenho@gmail.com>
2026-03-06 15:23:14 +0700

    feat(frontend): update routing to include Homepage / Cập nhật đường dẫn cho Trang chủ

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,12 +1,16 @@
+
 import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { Homepage } from './pages/homepage/homepage';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: Dashboard },
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+  { path: 'homepage', component: Homepage },
+  { path: 'dashboard', component: Dashboard },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
     { path: '**', redirectTo: 'dashboard' }
 ];
+
